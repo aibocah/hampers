bf5be1fa0e5043195003b707d9bd66fcd8b04097
aibocah <aibocah86@gmail.com>
2026-01-04 00:20:42 +0700

    Update modal.js

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,14 +3,37 @@ let selectedProduct = null;
 function openModal(product) {
   selectedProduct = product;
 
-  document.getElementById("modalTitle").innerText = product.name;
-  document.getElementById("modalPrice").innerText =
-    "Rp " + Number(product.price).toLocaleString("id-ID");
-  document.getElementById("modalDesc").innerText = product.description;
+  modalTitle.innerText = product.name;
+  modalPrice.innerText = "Rp " + product.price.toLocaleString("id-ID");
+  modalDesc.innerText = product.description;
 
-  document.getElementById("modal").style.display = "flex";
+  const slider = document.getElementById("modalSlider");
+  slider.innerHTML = "";
+
+  product.images.forEach((img, i) => {
+    slider.innerHTML += `
+      <img src="${img}" class="${i === 0 ? "active" : ""}" onclick="zoomImage('${img}')">
+    `;
+  });
+
+  initModalSlider();
+  modal.style.display = "flex";
 }
 
 function closeModal() {
-  document.getElementById("modal").style.display = "none";
+  modal.style.display = "none";
 }
+
+function initModalSlider() {
+  const imgs = document.querySelectorAll("#modalSlider img");
+  let i = 0;
+
+  if (imgs.length <= 1) return;
+
+  setInterval(() => {
+    imgs[i].classList.remove("active");
+    i = (i + 1) % imgs.length;
+    imgs[i].classList.add("active");
+  }, 2500);
+}
+
