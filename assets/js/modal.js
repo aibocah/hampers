816857b5389e05e90f3ae64c128dc68b9a90be9d
aibocah <aibocah86@gmail.com>
2026-01-04 00:31:30 +0700

    Update modal.js

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -36,4 +36,20 @@ function initModalSlider() {
     imgs[i].classList.add("active");
   }, 2500);
 }
+let selectedProduct = null;
+
+function openModal(product) {
+  selectedProduct = product;
+
+  document.getElementById("modalTitle").innerText = product.name;
+  document.getElementById("modalPrice").innerText =
+    "Rp " + Number(product.price).toLocaleString("id-ID");
+  document.getElementById("modalDesc").innerText = product.description;
+
+  document.getElementById("modal").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
 
